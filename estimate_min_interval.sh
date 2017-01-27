e37d077999b9c8e20428038a40bf098989d5f95d
Soramichi Akiyama <akiyama@m.soramichi.jp>
2017-01-27 12:27:02 +0900

    small fix

--- estimate_min_interval.sh
+++ estimate_min_interval.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
-PWD_OLD=`pwd` # Note: do not use name $PWD, as it's automatically overwritten by the shell
+# setting
 PERF="/home/soramichi/src/linux-4.9.4/tools/perf/perf"
 TARGET="/home/soramichi/src/cifar/svm/cifar_svm.py"
-TARGET_DIR=`dirname $TARGET`
+TARGET_ARGS=""
 COUNTERS="r81D0:pp"
+
+# never edit those
+PWD_OLD=`pwd` # Note: do not use name $PWD, as it's automatically overwritten by the shell
+TARGET_DIR=`dirname $TARGET`
 
 cd $TARGET_DIR # in case $TARGET accesses some data or libs with relative paths
 for c in 400000 200000 100000 50000 25000 12500; do
-    $PERF record -c $c -d -e $COUNTERS -o /dev/shm/perf.data.$c -- $TARGET > $PWD_OLD/$c.log 2>&1
+    $PERF record -c $c -d -e $COUNTERS -o /dev/shm/perf.data.$c -- $TARGET $TARGET_ARGS > $PWD_OLD/$c.log 2>&1
 done
