080b295b0e2d74db2fd3c4fc79c35b9deddcbc0e
Soramichi Akiyama <akiyama@m.soramichi.jp>
2017-01-31 17:12:43 +0900

    run.sh can run program using relative paths inside

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
+PWD_OLD=`pwd`
+
+# in case the target program uses data relative to its own pwd
+TARGET_DIR=`dirname $1` # assume $1 is the program name
+
 # for vector.o and hash.o
-LD_LIBRARY_PATH=. ./mat $@
+cd $TARGET_DIR
+LD_LIBRARY_PATH=$PWD_OLD $PWD_OLD/mat $@
