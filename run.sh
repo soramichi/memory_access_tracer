c6b305b7c0decc550dc6c6f4a5c56ed9ae11972d
Soramichi Akiyama <akiyama@m.soramichi.jp>
2017-02-02 11:36:44 +0900

    run.sh hanldes counter names properly

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+# args: $0 comm, $1 counters, $2 target program, $[3-N] arguments to the target program
+
 PWD_OLD=`pwd`
 
 # in case the target program uses data relative to its own pwd
-TARGET_DIR=`dirname $1` # assume $1 is the program name
+TARGET_DIR=`dirname $2` # assume $2 is the program name
 
 # for vector.o and hash.o
 cd $TARGET_DIR
