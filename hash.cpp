c403a82774c8acca3a7839be2db10659cde485c0
Soramichi Akiyama <akiyama@m.soramichi.jp>
2017-02-02 15:13:30 +0900

    typedef map<u64, u64> hash_t

--- hash.cpp
+++ hash.cpp
@@ -4,32 +4,33 @@
 
 #include "memory_access.h"
 
-typedef unsigned long u64;
-
 using namespace std;
 
+typedef unsigned long u64;
+typedef map<u64, u64> hash_t
+
 extern "C" void* create_hash(){
-  map<u64, u64>* ret = new map<u64, u64>();
+  hash_t* ret = new map<u64, u64>();
   return (void*)ret;
 }
 
 extern "C" void add_to_hash(void* _hash, u64 key, u64 value){
-  map<u64, u64>* hash = (map<u64, u64>*)_hash;
+  hash_t* hash = (map<u64, u64>*)_hash;
   hash->operator[](key) = value;
 }
 
 extern "C" u64 get_from_hash(void* _hash, u64 key){
-  map<u64, u64>* hash = (map<u64, u64>*)_hash;
+  hash_t* hash = (map<u64, u64>*)_hash;
   return hash->operator[](key);
 }
 
 extern "C" int get_size_of_hash(void* _hash){
-  map<u64, u64>* hash = (map<u64, u64>*)_hash;
+  hash_t* hash = (map<u64, u64>*)_hash;
   return hash->size();
 }
 
 extern "C" struct memory_access* get_memory_access(void* _hash){
-  map<u64, u64>* hash = (map<u64, u64>*)_hash;
+  hash_t* hash = (map<u64, u64>*)_hash;
   int i = 0;
   
   struct memory_access* ret = (struct memory_access*)malloc(sizeof(struct memory_access) * hash->size());
