6993178b2ed6ecd8da3b9009595ca1c3d9fcfe68
Soramichi Akiyama <akiyama@m.soramichi.jp>
2017-02-08 11:10:08 +0900

    take ip for the input

--- count_loads_to_pages.cpp
+++ count_loads_to_pages.cpp
@@ -8,6 +8,9 @@ typedef unsigned long long addr_t;
 
 const addr_t page_size = 4096;
 
+// Usage: perf script -F addr,ip -i perf.data
+// Note that addr and ip are displayed in this order no matter what the order after -F is.
+// (-F addr,ip and -F ip,addr yield exactly the same output)
 int main(void){
   string str;
   map<addr_t, int> count;
@@ -15,8 +18,21 @@ int main(void){
   cout << "start aggregating the events" << endl;
   
   while(getline(cin, str)){
-    addr_t addr = strtoll(str.c_str(), NULL, 16);
+    addr_t addr, ip;
+    const char* ptr = str.c_str();
+    int index = 0;
 
+    // skip heading spaces
+    while(ptr[index] == ' ')
+      index++;
+    addr = strtoll(ptr+index, NULL, 16);
+
+    // skip heading spaces
+    while(ptr[index] == ' ')
+      index++;
+    ip = strtoll(ptr+index, NULL, 16);
+
+    // inside the kernel, just skip this sample
     if(addr == 0ull)
       continue;
 
